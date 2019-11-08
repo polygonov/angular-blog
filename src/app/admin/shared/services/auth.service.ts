16e037c54c9144cab713bee2399ef8289fbe1a38
ipolyakov <ipolyakov@bcc.ru>
2019-11-08 18:04:56 +0300

    auth complete

---
+++ src/app/admin/shared/services/auth.service.ts
@@ -0,0 +1,69 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {FbAuthResponse, User} from '../../../shared/interfaces';
+import {Observable, Subject, throwError} from 'rxjs';
+import {environment} from '../../../../environments/environment';
+import {catchError, tap} from 'rxjs/operators';
+
+@Injectable()
+export class AuthService {
+
+  public errors$: Subject<string> = new Subject<string>()
+
+  constructor(private http: HttpClient) {}
+
+  get token(): string {
+    const expDate = new Date(localStorage.getItem('fb-token-exp'))
+    if (new Date() > expDate){
+      this.logout()
+      return null
+    }
+    return localStorage.getItem('fb-token')
+  }
+
+  login(user: User): Observable<any> {
+    user.returnSecureToken = true
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap(this.setToken),
+        catchError(this.handleError.bind(this))
+      )
+  }
+
+  logout() {
+    this.setToken(null)
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.token
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    const { message } = error.error.error
+
+    switch (message) {
+      case 'EMAIL_NOT_FOUND':
+        this.errors$.next('Email не найден')
+        break
+      case 'INVALID_EMAIL':
+        this.errors$.next('Неверный email')
+        break
+      case 'INVALID_PASSWORD':
+        this.errors$.next('Неверный пароль')
+        break
+    }
+
+    return throwError(error)
+  }
+
+  private setToken(responce: FbAuthResponse | null) {
+    if (responce) {
+      const expDate = new Date(new Date().getTime() + +responce.expiresIn * 1000)
+      localStorage.setItem('fb-token', responce.idToken)
+      localStorage.setItem('fb-token-exp', expDate.toString())
+    } else {
+      localStorage.clear()
+    }
+
+  }
+}
